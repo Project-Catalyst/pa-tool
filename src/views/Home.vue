20678c64e9419952ccc63c9f4c630885308401fc
Lucio Baglione <coire1@gmail.com>
2021-04-28 22:19:08 +0200

    Update links.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,7 +24,7 @@
       </p>
       <p>
         <strong>Disclaimer</strong>: the data about the assessment count is
-        <strong>not updated</strong> in real time and therefore is likely 
+        <strong>not updated</strong> in real time and therefore is likely
         different from those shown in IdeaScale.
       </p>
     </b-message>
@@ -32,8 +32,8 @@
       <p><b>Useful links for Community Advisors</b></p>
       <p>
         Watch <a href="https://www.youtube.com/watch?v=cOXRP68kj7I&t=2679s" target="_blank">this video</a> for CA guidelines<br />
-        Please read the <a href="https://docs.google.com/document/d/16aq9dNudJ5S3TEVQhBgRznTCoaF8SQezyActtVhec8E/edit" target="_blank">onboarding document</a><br />
-        Read our <a href="https://docs.google.com/document/d/1QkdaFK1tigrSI40iMeV3UP9GyTGsoqVmCUp7OJz0WFs/edit" target="_blank">Community advisor guidelines</a><br />
+        Please read the <a href="https://docs.google.com/document/d/16aq9dNudJ5S3TEVQhBgRznTCoaF8SQezyActtVhec8E" target="_blank">onboarding document</a><br />
+        Read our <a href="https://docs.google.com/document/d/1Fn1CQHK_TNSaybQtnxvI9DZJ9PAufZelBEfOLWbp-gw" target="_blank">Community Advisor Assessment Guide</a><br />
         Join the <a href="https://t.me/CatalystCommunityAdvisors" target="_blank">community advisor TG support channel</a>
       </p>
     </b-message>
