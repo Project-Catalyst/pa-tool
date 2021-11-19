d1e233e3c5fd3d86f76ed7d8b638577cf1346fd1
Lucio Baglione <coire1@gmail.com>
2021-11-19 18:53:48 +0100

    Adjust environments.

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
+let publicPath = ''
+if (process.env.APP_ENV === 'production') {
+  publicPath = '/ca-tool/'
+}
+if (process.env.APP_ENV === 'staging') {
+  publicPath = '/'
+}
 module.exports = {
-  publicPath: (process.env.NODE_ENV === 'production') ? '/ca-tool/' : ''
+  publicPath: publicPath
 }
