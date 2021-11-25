566e8a4338827ca9c1f3ac82cd33d63ae9ce49d1
Lucio Baglione <coire1@gmail.com>
2021-11-25 23:17:28 +0100

    Optimize buefy loading.

--- src/main.js
+++ src/main.js
@@ -1,19 +1,16 @@
 import Vue from 'vue'
-import Buefy from "buefy";
 import Clipboard from 'v-clipboard'
 import App from './App.vue'
 import router from './router'
 import store from "./store";
+import './plugins/buefy'
 import './plugins/axios'
 import "./plugins/vue-papa-parse";
 
-import "buefy/dist/buefy.css";
-import 'vue-simple-markdown/dist/vue-simple-markdown.css'
 import "./assets/sass/main.scss";
 
 Vue.config.productionTip = false
 Vue.use(Clipboard)
-Vue.use(Buefy);
 
 new Vue({
   router,
---
+++ src/plugins/buefy.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import {
+  Button,
+  Navbar,
+  Message,
+  Taginput,
+  Field,
+  Input,
+  Modal,
+  Icon,
+  Progress,
+  Tooltip,
+  Tag,
+  Rate,
+  Checkbox,
+  Dialog
+} from "buefy";
+// import { Buefy } from "buefy"
+
+import "buefy/dist/buefy.css";
+
+Vue.use(Button);
+Vue.use(Navbar);
+Vue.use(Message);
+Vue.use(Taginput);
+Vue.use(Field);
+Vue.use(Input);
+Vue.use(Modal);
+Vue.use(Icon);
+Vue.use(Progress);
+Vue.use(Tooltip);
+Vue.use(Tag);
+Vue.use(Rate);
+Vue.use(Checkbox);
+Vue.use(Dialog);
+//Vue.use(Buefy);
