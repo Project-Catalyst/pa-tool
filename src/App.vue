8d123f2ae3984e4208a015094d82f497fbf3d3a4
Lucio Baglione <coire1@gmail.com>
2021-12-09 12:25:17 +0100

    Update preview message.

--- src/App.vue
+++ src/App.vue
@@ -44,7 +44,7 @@
       <div class="content-wrapper">
         <b-message type="is-warning">
           This is a preview of the CA-tool for Fund 7. Data from proposals and CA Resources are not final!<br />
-          The assess stage will start on <b>09 Dec 2021 11:00 UTC</b> but you can navigate and use the tool to get familiar with it and test it.
+          The assess stage will start <b>soon!</b> You can navigate and use the tool to get familiar with it and test it.
         </b-message>
         <router-view/>
       </div>
