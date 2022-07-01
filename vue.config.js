c8b73408f990f797c5804f174f1cbcfdeba48ddb
Lucio Baglione <coire1@gmail.com>
2022-07-01 08:52:04 +0200

    Change url config.

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 let publicPath = ''
 if (process.env.APP_ENV === 'production') {
-  publicPath = '/ca-tool/'
+  publicPath = '/pa-tool/'
 }
 if (process.env.APP_ENV === 'staging') {
   publicPath = '/'
